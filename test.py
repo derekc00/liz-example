714ff7a1738cbf996afd5d6639a07654de69b9ff
Derek Chang <derekchangc00@gmail.com>
2025-01-04 21:32:48 -0800

    first commit

---
+++ test.py
@@ -0,0 +1,21 @@
+x = 4
+
+my_var = 10       # int
+my_string = "Hi"  # str
+my_bool = True    # bool (True = 1 / False = 0)
+
+if my_bool == True:
+  print("my_bool is True")
+
+
+# sdfsaoifhasfsfj 
+# this is a helpful note for anyone reading this code
+if x > 0:
+  print("x is positive")
+  print("hi:)")
+  print("hi:)")
+  print("hi:)")
+  print("hi:)")
+  print("hi:)")
+else:
+  print("x is negative")
